200ccf43b4d54e6213eba8bf36d23e467cf3ff1f
DavidYo <lturtsamuel@gmail.com>
2017-07-23 23:31:45 +0800

    增加可查詢看板渲染規則的 api

--- src/rule/router.js
+++ src/rule/router.js
@@ -18,24 +18,39 @@ function ruleDone(rules) {
 	return true;
 }
 
+async function findRule(board, rules) {
+	while (true) {
+		for (let key of RULES) {
+			if (!rules[key]) rules[key] = board[key];
+		}
+		if (ruleDone(rules) || board.isRoot) break;
+		else board = await findBoard(board.mather);
+	}
+}
+
 router.get("/article/:article", async function(req, res) {
 	try {
 		let article_id = req.params.article;
 		let article = await db.Article.findOne({ _id: article_id }).lean().exec();
 		let board = await findBoard(article.board);
-
 		let rules = {};
-		for (let key of RULES) rules[key] = null;
-		if (article.renderComment) {
-			rules["renderComment"] = article.renderComment;
-		}
-		while (true) {
-			for (let key of RULES) {
-				if (!rules[key]) rules[key] = board[key];
-			}
-			if (ruleDone(rules) || board.isRoot) break;
-			else board = await findBoard(board.mather);
-		}
+		rules["renderComment"] = article.renderComment;
+
+		await findRule(board, rules);
+		res.json(rules);
+	} catch(err) {
+		console.log(err);
+		res.send("FAIL");
+	}
+});
+
+router.get("/board/:board", async function(req, res) {
+	try {
+		let board_id = req.params.board;
+		let board = await db.Board.findOne({ _id: board_id }).lean().exec();
+		let rules = {};
+
+		await findRule(board, rules);
 		res.json(rules);
 	} catch(err) {
 		console.log(err);
