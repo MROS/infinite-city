682e8ab8c62a7c51b0dc2ca1a62c34ff8fdf2d66
MROS <yc1043@gmail.com>
2017-08-14 01:50:15 +0800

    修正拼字錯誤、根據環境送出不同連結

--- src/util/mail_util.js
+++ src/util/mail_util.js
@@ -1,12 +1,24 @@
 const Mailgun = require("mailgun-js");
 
-const config = require("./mail_config.js");
-const mailgun = new Mailgun({ apiKey: config.api_key, domain: config.domain });
+const config = require("../config.js");
+const mail_config = require("./mail_config.js");
+const mailgun = new Mailgun({ apiKey: mail_config.api_key, domain: mail_config.domain });
 
 function _generateVerificationMsg(user_id, guid) {
-	let url = `http://city-of-infinity.com/app/verification?guid=${guid}`;
+	let url = (() => {
+		let env = require("optimist").argv.env || process.env.env || "dev";
+		switch (env) {
+			case "dev":
+			case "test":
+				return `http://localhost:${config.PORT}/app/verification?guid=${guid}`
+			case "production":
+				return  `http://city-of-infinity.com/app/verification?guid=${guid}`;
+			default:
+				throw `未知的環境：${env}`;
+		}
+	})();
 	return `<h1>${user_id}，歡迎來到∞無限城∞！</h1>
-	<p>恭禧你，即將成為全台最二社群網站的一員。請點擊下列網址開通你的帳號，開始享受你的無限人生吧！</p>
+	<p>恭喜你，即將成為全台最二社群網站的一員。請點擊下列網址開通你的帳號，開始享受你的無限人生吧！</p>
 	<a href="${url}">${url}</a>
 	<p>我們在巴比倫城的頂點等著你</p>
 	<p>⚡雷帝 👁邪眼男敬上</p>`;
@@ -14,7 +26,7 @@ function _generateVerificationMsg(user_id, guid) {
 
 function sendVerificationMail(user_id, guid, email) {
 	let data = {
-		from: config.from,
+		from: mail_config.from,
 		to: email,
 		subject: `${user_id}，歡迎來到∞無限城∞！`,
 		html: _generateVerificationMsg(user_id, guid)
