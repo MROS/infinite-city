f35f768730e543d54a0f1456977f36bd181ea96e
MROS <yc1043@gmail.com>
2018-09-07 17:45:31 +0800

    可在 express 中 gzip 壓縮資源

--- src/app.js
+++ src/app.js
@@ -6,12 +6,21 @@ const config = require("./config.js");
 
 let app = express();
 
+// 在開發環境使用 compression 套件來壓縮。生產環境則在 反向代理時進行壓縮，因此無需該套件。
+if (config.env == "dev") {
+	const compression = require("compression");
+	app.use(compression());
+}
+
+// log
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
+// 解析 body
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+// 以 redis 做 cookie 快取
 const session = require("express-session");
 const RedisStore = require("connect-redis")(session);
 
